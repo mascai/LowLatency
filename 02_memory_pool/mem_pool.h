7d09864bd14f21daf5f065f3a9185b64f5b62427
mascai <msumoskalenko@gmail.com>
2024-05-13 21:30:58 +0300

    Add logger

--- 02_memory_pool/mem_pool.h
+++ 02_memory_pool/mem_pool.h
@@ -3,7 +3,7 @@
 #include <vector>
 #include <string>
 
-#include "macros.h"
+#include "../macros.h"
 
 
 namespace Common {
--- 03_lock_free_queue/lock_free_queue.h
+++ 03_lock_free_queue/lock_free_queue.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include <iostream>
 
-#include "macros.h"
+#include "../macros.h"
 
 
 namespace Common {
